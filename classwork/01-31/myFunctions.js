4a67b2354a0d7e410e66eb286f8379b07e90cf01
rathod-di <rathod.di@northeastern.edu>
2023-01-31 21:11:30 -0500

    cw

---
+++ classwork/01-31/myFunctions.js
@@ -0,0 +1,14 @@
+// A series of custom funcctions, This file can be considered
+
+
+
+function sumAB(A,B) {
+
+    return A+B;
+
+}
+function randomPosition(){
+
+    return Math.random()*140+70
+}
+
