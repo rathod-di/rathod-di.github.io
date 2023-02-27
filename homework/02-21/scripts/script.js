2a8ce79b1d2a00497fad0ba0d9b296f73ea3601a
rathod-di <rathod.di@northeastern.edu>
2023-02-27 15:42:45 -0500

    hw

--- homework/02-21/scripts/script.js
+++ homework/02-21/scripts/script.js
@@ -1,188 +1,151 @@
 /*
-
-ARTG5330 Visualization Technolgies 1
-February 21,2023
-Spring Semester
-Week 7
-
+    ARTG5330 Visualization Technologies 1
+    February 21, 2023
+    Spring Semester
+    Week 7
+    - What is the "./data/gapminder.csv" inside of the parentheses 
+    for d3.csv() referring to?
+    It is referring to a dataset given in csv filetype provided by
+    Gapminder that is stored locally inside the folder called "data".
+    You need to provide the exact local path to that csv file to
+    fetch it with the d3.csv() method.
+    - The parameter named `data` inside of the function expression .then()
+    The parameter named `data` binds to the csv file
+    you fetch with the d3.csv method. Thus, it refers
+    to the gapminder.csv file.
+    - What kind of JavaScript data structure is `data`?
+    An array of objects.
+    - Where does the entire d3.csv().then() pattern
+        open and close in this document?
+    Use the VSCode interface to locate the opening
+    and closing of the d3.csv().then() pattern.
+    You may find it useful to examine the contents
+    of `data` with console.log(data).
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+d3.csv("./data/gapminder.csv").then(function(data) {
+
+    /*
+    1. DEFINE DIMENSIONS OF SVG + CREATE SVG CANVAS
+        - What is document.querySelector("#chart") doing?
+        The document.querySelector() method is returning the
+        FIRST element within your html document that matches the
+        specified CSS selector string, i.e., the ID "#chart". 
+        See here for more info on CSS selectors: 
+        https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors
+        - `clientWidth` and `clientHeight` are properties of
+            elements in the DOM (Document Object Model).
+            What do these properties measure?
+        These properties measure the inner width and inner height
+        of an HTML element in pixels. In this example, they are 
+        measuring the width and height of the div element in the html page
+        that encapsulates the chart. Note that these values are calculated
+        according to how the chart is displayed in the browser. Thus, the 
+        values will change if you resize your browser's window. You can
+        test this behavior using console.log() prints.
+        Note: document.clientWidth is different from window.innerWidth
+        The d
+    */
+
+    const width = document.querySelector("#chart").clientWidth;
+    const height = document.querySelector("#chart").clientHeight;
+
+    // Initializing the viewport of the SVG canvas
+    // An SVG Canvas's Viewport has a "width" and "height"
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+
+    /* 
+    2. FILTER THE DATA 
+    
+    This data set is large and includes data from multiple years.
+    Let's filter the data to only select data for the United States,
+    and then subsequently use those data to draw the bar chart.
+    To filter the data, we can use the .filter() method for arrays.
+    `.filter()` is a JavaScript array method.
+    - What does this method do/how does this method work?
+        (What do we get back from using this method?)
+    - Inside the parentheses for .filter(), there is
+        a function expression with a parameter
+        named `d`. What is `d` a reference to?
+    The .filter() method returns a new array of objects
+    that is a SUBSET of the array of objects stored in `data`. 
+    The purpose of the method is to filter out rows of the original
+    dataset that do not satisfy a specific "test". This test is
+    given here inside the custom function, and it is in the form of an exact 
+    equality (===) between two strings (see below). 
+    The parameter named `d` is a reference to the rows
+    of the input CSV file.
+    
+    - Does that parameter *have to be* named `d`?
+        Can it be named something else?
+    No. You can use a different symbol.
+    
+    - What is the purpose of the statement inside
+        the function expression? What is this doing?
+        return d.country === 'United States';
+    As you access each row of the csv file using the
+    `d` paramater, you retrieve the value d.country for
+    each row and test if it is (strictly) equal to the
+    provided string, `United States`. The row passes the
+    test if it is, otherwise it is ignored.
+    - Why are we storing the result of data.filter(...)
+        inside a variable (filtered_data)?
+    We are storing the result in a new variable called
+    filtered_data because we want to keep the filtered data
+    separate from the original data we have stored in `data`.
+    Also, we want to reuse the filtered_data in multiple
+    locations in the code (see below).
+    */
+
+    let filtered_data = data.filter(function(d) {
+
+        return d.country === 'United States';
+
+    });
+
+
+    /*
+    3. DETERMINE MIN AND MAX VALUES OF VARIABLES
+    In the following section, we'll use the methods d3.min() and d3.max()
+    to calculate minimum and maximum values of the variables in our data set.
+    Note that to keep things clean, we're organizing the minimum and maximum
+    values inside of objects, and storing those min/max values in properties
+    named inside those objects. This helps make it easier to refer to these
+    values later in our code.
+        - What does d3.min() do? What does d3.max() do?
+            What are the 2 arguments we supply to d3.min()/d3.max()?
+        The functions d3.min() and d3.max() return the minimum and
+        maximum numerical values stored in an array. Here, the first
+        argument is the filtered dataset we created just above and the
+        second argument is an ACCESSOR function. The purpose of this
+        function is to specify which exact column in the dataset
+        contains the numerical values we are interested in. So, `d`
+        refers to the rows of the filtered dataset, and the function
+        accesses each row and returns specifically the value for the
+        variable `lifeExp`. Thus, min and max here are the minimum
+        and maximum numerical values in the column for `lifeExp`.
+        Note, we also coerce the strings into numbers to make this work.
+        Remember that d3.csv() loads the data in the form of strings.
+        To work with numbers represented as strings, you must explicitly
+        cast them back into numbers.
+        - In the second argument for both d3.min() and d3.max(),
+            the function expression has a parameter named `d`.
+            What is `d` a reference to?
+        See above answer.
+        - Why is there a plus sign (+) in front of d.lifeExp?
+        See above answer.
+    */
+
+    const lifeExp = {
+        
+        min: d3.min(filtered_data, function(d) { return +d.lifeExp; }),
+        max: d3.max(filtered_data, function(d) { return +d.lifeExp; })
+
+    };
 /*
 4. CREATE SCALES
 We'll use the computed min and max values to create scales for
@@ -223,21 +186,20 @@ our scatter plot.
 */
 
 const margin = {
-    top: 50,
-    left: 100,
-    right: 50,
+    top: 50, 
+    left: 100, 
+    right: 50, 
     bottom: 100
 };
 
-
 const xScale = d3.scaleBand()
-.domain(["1952","1957","1962","1967","1972","1977","1982","1987","1992","1997","2002","2007"])
-.range([margin.left, width-margin.right])
-.padding(0.5);
+    .domain(["1952","1957","1962","1967","1972","1977","1982","1987","1992","1997","2002","2007"])
+    .range([margin.left, width - margin.right])
+    .padding(0.5);
 
 const yScale = d3.scaleLinear()
-.domain([50, lifeExp.max])
-.range([height-margin.bottom, margin.top]);
+    .domain([50, lifeExp.max])
+    .range([height - margin.bottom, margin.top]);
 
 /*
 5. DRAW AXES
