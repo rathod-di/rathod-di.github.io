cf5b06fa625aead4f9cba097867e2953ce3e98d2
rathod-di <rathod.di@northeastern.edu>
2023-02-28 13:08:18 -0500

    hw

--- homework/02-28/script/script.js
+++ homework/02-28/script/script.js
@@ -1,4 +1,5 @@
 d3.csv("./data/gapminder.csv").then(function(data) {
+
   const width = document.querySelector("#chart").clientWidth;
   const height = document.querySelector("#chart").clientHeight;
 
@@ -24,7 +25,7 @@ d3.csv("./data/gapminder.csv").then(function(data) {
   };
 
   const xScale = d3.scaleBand()
-      .domain(["1952", "1957", "1962", "1967", "1972", "1977", "1982", "1987", "1992", "1997", "2002", "2007"])
+      .domain(["1952","1957","1962","1967","1972","1977","1982","1987","1992","1997","2002","2007"])
       .range([margin.left, width - margin.right])
       .padding(0.5);
 
@@ -32,35 +33,46 @@ d3.csv("./data/gapminder.csv").then(function(data) {
       .domain([50, lifeExp.max])
       .range([height - margin.bottom, margin.top]);
 
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  svg.append("g")
+      .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("transform", "translate(" + margin.left + ",0)")
+      .call(yAxis);
+
   const points = svg.selectAll("rect")
       .data(filtered_data)
       .enter()
       .append("rect")
-      .attr("x", function(d) { return xScale(d.year); })
-      .attr("y", function(d) { return yScale(d.gdpPercap); })
-      .attr("width", xScale.bandwidth())
-      .attr("height", function(d) { return height - (margin.bottom + yScale(d.gdpPercap)) })
-      .attr("fill", function(d) {
-          switch(d.year) {
-              case "1952":
-                  return "blue";
-              case "1972":
-                  return "red";
-              default:
-                  return "green";
-          }
-      });
+          .attr("x", function(d) { return xScale(d.year); })
+          .attr("y", function(d) { return yScale(d.gdpPercap); })
+          .attr("width", xScale.bandwidth())
+          .attr("height", function(d) { return height - (margin.bottom + yScale(d.gdpPercap)) })
+          .attr("fill", function(d) {
+            if (d.year >= "1952" && d.year <= "1967") {
+                return "red";
+            } else if (d.year >= "1972" && d.year <= "1987") {
+                return "blue";
+            } else {
+                return "green";
+            }
+        });
 
   const xAxisLabel = svg.append("text")
-      .attr("class", "axisLabel")
-      .attr("x", width / 2)
-      .attr("y", height - margin.bottom / 2)
+      .attr("class","axisLabel")
+      .attr("x", width/2)
+      .attr("y", height-margin.bottom/2)
       .text("Year");
 
   const yAxisLabel = svg.append("text")
-      .attr("class", "axisLabel")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -height / 2)
-      .attr("y", margin.left / 2)
+      .attr("class","axisLabel")
+      .attr("transform","rotate(-90)")
+      .attr("x", -height/2)
+      .attr("y", margin.left/2)
       .text("GDP (Years)");
+
 });
