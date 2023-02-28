959e49d2eee891d9318642fafac097c55d725c85
rathod-di <rathod.di@northeastern.edu>
2023-02-28 10:18:13 -0500

    hw

--- homework/02-28/script/script.js
+++ homework/02-28/script/script.js
@@ -1,64 +1,50 @@
-d3.csv("data/gapminder.csv").then(function(data) {
-    const width = document.querySelector("#chart").clientWidth;
-    const height = document.querySelector("#chart").clientHeight;
-  
-    // Group the data by year and continent
-    const groupedData = d3.group(data, d => d.year, d => d.continent);
-  
-    // Get the maximum GDP per capita across all years and continents
-    const maxGdp = d3.max(data, d => +d.gdpPercap);
-  
-    const margin = {top: 50, left: 100, right: 50, bottom: 100};
-  
-    const xScale = d3.scaleBand()
-      .domain(groupedData.keys())
-      .range([margin.left, width - margin.right])
-      .padding(0.1);
-  
-    const yScale = d3.scaleLinear()
-      .domain([0, maxGdp])
-      .range([height - margin.bottom, margin.top]);
-  
-    const colorScale = d3.scaleOrdinal()
-      .domain(groupedData.keys())
-      .range(d3.schemeCategory10);
-  
-    const xAxis = svg.append("g")
-      .attr("class", "axis")
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom().scale(xScale));
-  
-    const yAxis = svg.append("g")
-      .attr("class", "axis")
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft().scale(yScale));
-  
-    const bars = svg.selectAll(".bar")
-      .data(groupedData)
-      .enter()
-      .append("g")
-      .attr("class", "bar")
-      .attr("transform", d => `translate(${xScale(d[0])},0)`)
-      .selectAll("rect")
-      .data(d => d[1])
-      .enter()
-      .append("rect")
-      .attr("x", d => xScale.bandwidth() / d[1].length * d[1].index)
-      .attr("y", d => yScale(+d.gdpPercap))
-      .attr("width", xScale.bandwidth() / d[1].length)
-      .attr("height", d => height - margin.bottom - yScale(+d.gdpPercap))
-      .attr("fill", d => colorScale(d[0]));
-  
-    const xAxisLabel = svg.append("text")
-      .attr("class", "axisLabel")
-      .attr("x", width / 2)
-      .attr("y", height - margin.bottom / 2)
-      .text("Year");
-  
-    const yAxisLabel = svg.append("text")
-      .attr("class", "axisLabel")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -height / 2)
-      .attr("y", margin.left / 2)
-      .text("GDP Per Capita");
+d3.csv("data/gapminder.csv", function(data) {
+  // Convert year and gdpPercap to numbers
+  data.forEach(function(d) {
+    d.year = +d.year;
+    d.gdpPercap = +d.gdpPercap;
   });
+
+  // Group data by year and continent
+  var nestedData = d3.group(data, d => d.year, d => d.continent);
+
+  // Get unique continents
+  var continents = d3.map(data, function(d){return(d.continent)}).keys()
+
+  // Define x and y scales
+  var x = d3.scaleBand()
+    .domain(d3.map(data, function(d){return(d.year)}).keys())
+    .range([0, width])
+    .padding([0.2]);
+
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d){return(d.gdpPercap)})])
+    .range([height, 0]);
+
+  // Define color scale
+  var color = d3.scaleOrdinal()
+    .domain(continents)
+    .range(d3.schemeCategory10);
+
+  // Create bars
+  svg.append("g")
+    .selectAll("g")
+    .data(nestedData)
+    .join("g")
+      .attr("fill", function(d) { return color(d[0]); })
+    .selectAll("rect")
+    .data(function(d) { return d[1]; })
+    .join("rect")
+      .attr("x", function(d) { return x(d.year); })
+      .attr("y", function(d) { return y(d.gdpPercap); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.gdpPercap); });
+
+  // Add x and y axes
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  svg.append("g")
+    .call(d3.axisLeft(y));
+});
