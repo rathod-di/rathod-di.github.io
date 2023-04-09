3557d680a190f2286819181716378a7b5e78e812
rathod-di <rathod.di@northeastern.edu>
2023-04-09 19:02:08 -0400

    time

---
+++ homework/03-28/script/TimeCrime.js
@@ -0,0 +1,37 @@
+d3.csv("TimeCrime.csv").then(function(data) {
+    var trace1 = {
+      x: data.map(function(d) { return d.time; }),
+      y: data.map(function(d) { return d.day; }),
+      type: 'bar',
+      name: 'Day'
+    };
+  
+    var trace2 = {
+      x: data.map(function(d) { return d.time; }),
+      y: data.map(function(d) { return d.mid_day; }),
+      type: 'bar',
+      name: 'Mid-Day'
+    };
+  
+    var trace3 = {
+      x: data.map(function(d) { return d.time; }),
+      y: data.map(function(d) { return d.night; }),
+      type: 'bar',
+      name: 'Night'
+    };
+  
+    var layout = {
+      title: 'Crime Rates by Time of Day',
+      xaxis: {
+        title: 'Time of Day'
+      },
+      yaxis: {
+        title: 'Crime Rate'
+      }
+    };
+  
+    var data = [trace1, trace2, trace3];
+  
+    Plotly.newPlot('chart', data, layout);
+  });
+  
